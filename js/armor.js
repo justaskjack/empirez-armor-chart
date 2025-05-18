0bfb1066c6910a4231863ccc9cbc4f36ff2412fa
justaskjack <12213337+justaskjack@users.noreply.github.com>
2025-05-18 11:14:15 -0400

    Update armor.js

--- js/armor.js
+++ js/armor.js
@@ -1,11 +1,15 @@
+// Load the armor data from armor.json
 fetch('data/armor.json')
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById('armor-container');
 
     data.forEach(armor => {
+      // Create the armor card container
       const card = document.createElement('div');
       card.className = 'armor-card';
+
+      // Populate the inner HTML with Lightbox-enabled image and armor details
       card.innerHTML = `
         <a href="images/${armor.image}" data-lightbox="armor" data-title="${armor.name} - ${armor.type}">
           <img src="images/${armor.image}" alt="${armor.name}" class="armor-img" />
@@ -16,6 +20,11 @@ fetch('data/armor.json')
         <p><strong>Rarity:</strong> ${armor.rarity}</p>
         <p><strong>Location:</strong> ${armor.location}</p>
       `;
+
+      // Add the card to the page
       container.appendChild(card);
     });
+  })
+  .catch(error => {
+    console.error("Error loading armor data:", error);
   });
