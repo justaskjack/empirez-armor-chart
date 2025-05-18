142ae9c859cc24b4c4352fcf0e4e013910512a67
justaskjack <12213337+justaskjack@users.noreply.github.com>
2025-05-18 10:47:35 -0400

    Add files via upload

--- js/armor.js
+++ js/armor.js
@@ -1,2 +1,19 @@
-// JS test
-console.log("Armor script loaded");
+fetch('data/armor.json')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('armor-container');
+
+    data.forEach(armor => {
+      const card = document.createElement('div');
+      card.className = 'armor-card';
+      card.innerHTML = `
+        <img src="images/${armor.image}" alt="${armor.name}" />
+        <h2>${armor.name}</h2>
+        <p><strong>Type:</strong> ${armor.type}</p>
+        <p><strong>Protection:</strong> ${armor.protection}</p>
+        <p><strong>Rarity:</strong> ${armor.rarity}</p>
+        <p><strong>Location:</strong> ${armor.location}</p>
+      `;
+      container.appendChild(card);
+    });
+  });
